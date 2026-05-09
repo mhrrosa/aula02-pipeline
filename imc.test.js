6464536edb0101dda77f45e3a181839d83ed3433
GustavoFoss <fossgustavo8@gmail.com>
2026-05-09 10:09:40 -0300

    teste falho

--- imc.test.js
+++ imc.test.js
@@ -11,3 +11,7 @@ test('não deve retornar um valor incorreto de IMC', () => {
 test('o resultado deve ser maior que 20', () => {
     expect(calcularIMC(80, 1.80)).toBeGreaterThan(20);
 });
+
+test('comparação sem sentido nenhum', () => {
+    expect(calcularIMC(80, 1.80)).toEqual({ imc: 'banana' });
+});
